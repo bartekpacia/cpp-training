2c75cb4a8fd4954f6f6d4a6436ac88922b98f848
Bartek Pacia <barpac02@gmail.com>
2019-06-09 00:16:07 +0200

    added dummy task_2_5 from excercise book

---
+++ excercise_book/task_2_5/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <random>
+
+
+/**
+ * I didn't do a task, I was just making fun XD.
+ */
+
+using namespace std;
+
+int main()
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist (1, 160);
+
+    int iq = dist(gen);
+
+    cout << "IQ Oli to " << iq << endl;
+    return 0;
+}
