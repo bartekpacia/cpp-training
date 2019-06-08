a4253cb2cfecf2dc9704e3ee1ab932e7b769a97f
Bartek Pacia <barpac02@gmail.com>
2019-06-08 11:32:28 +0200

    Added dummy task 2.1 from excercise book

---
+++ excercise_book/task_2_1/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * Task 2.1
+ * Write a program which checks if 
+ * a rectangular triangle can be created 
+ * from sides a, b and c.
+ **/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, c;
+
+    cout << "a";
+
+    return 0;
+}
