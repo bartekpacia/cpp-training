1d71d36df96acc4e726a3c9d38084c969ff7cbd4
Bartek Pacia <barpac02@gmail.com>
2019-06-09 13:07:39 +0200

    added task_1_3 from the excercise book

---
+++ excercise_book/task_1_3/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * Write a program which displays the square root
+ * of pi.
+ */
+
+#include <math.h>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float pi = M_PI;
+
+    cout << fixed << setprecision(2) << sqrt(pi) << endl;
+
+    return 0;
+}
