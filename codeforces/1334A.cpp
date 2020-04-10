5b9c9a521bde372384dc89176442b4bd15a7a355
Bartek Pacia <barpac02@gmail.com>
2020-04-10 18:09:31 +0200

    done some task, example tests pass

---
+++ codeforces/1334A.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int t = 0; t < T; t++) {
+        bool valid = true;
+        int n;
+        cin >> n;
+
+        int p, c;
+        int prev_p, prev_c;
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                cin >> prev_p >> prev_c;
+            } else if (i == 1) {
+                cin >> p >> c;
+
+                if (p < prev_p || c < prev_c) {
+                    valid = false;
+                }
+
+                if (c > p) {
+                    valid = false;
+                }
+            } else {
+                prev_p = p;
+                prev_c = c;
+                cin >> p >> c;
+
+                if (p < prev_p || c < prev_c) {
+                    valid = false;
+                }
+
+                if (c > p) {
+                    valid = false;
+                }
+            }
+        }
+
+        if (valid) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
