707f82bd816da1a05f81ec03b5ad50c113ab2471
Bartek Pacia <barpac02@gmail.com>
2020-04-25 21:51:34 +0200

    nothing

---
+++ project_euler/0011.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int grid_size = 20;
+
+    vector<vector<int>> vec = {};
+
+    int column = 1;
+    int row = 1;
+
+    return 0;
+}
